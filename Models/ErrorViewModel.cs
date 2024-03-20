94b9710fb91ccc4c41664beb31efadf6009ee2c8
Muhammad Taha <39196340+Tahawahid@users.noreply.github.com>
2024-03-20 12:00:18 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace kashishkumari.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
